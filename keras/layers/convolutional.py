7c2bd7bb4a231dd17eb9766d374941d80910710d
Gijs van Tulder <gvtulder@gmail.com>
2016-11-22 12:20:53 +0100

    Deconvolution layers accept output_shape without batch size.

--- keras/layers/convolutional.py
+++ keras/layers/convolutional.py
@@ -619,6 +619,9 @@ class Deconvolution2D(Convolution2D):
             dim_ordering = K.image_dim_ordering()
         if border_mode not in {'valid', 'same', 'full'}:
             raise Exception('Invalid border mode for Deconvolution2D:', border_mode)
+        if len(output_shape) == 3:
+            # missing the batch size
+            output_shape = (None,) + tuple(output_shape)
 
         self.output_shape_ = output_shape
 
@@ -1374,6 +1377,9 @@ class Deconvolution3D(Convolution3D):
             dim_ordering = K.image_dim_ordering()
         if border_mode not in {'valid', 'same', 'full'}:
             raise Exception('Invalid border mode for Deconvolution3D:', border_mode)
+        if len(output_shape) == 4:
+            # missing the batch size
+            output_shape = (None,) + tuple(output_shape)
 
         self.output_shape_ = output_shape
 
